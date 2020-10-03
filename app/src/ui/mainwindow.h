a498cdf1ae4b6f38bbc7a9cfa13f28fd02c914fd
Jalen Adams <jalen@jalenkadams.me>
2020-10-03 18:08:43 -0400

    Document: add license header to source files

--- app/src/ui/mainwindow.h
+++ app/src/ui/mainwindow.h
@@ -1,3 +1,26 @@
+/******************************************************************************
+ * mainwindow.h : The main UI window
+ * ****************************************************************************
+ * Copyright (C) 2020 Jalen Adams
+ *
+ * Authors: Jalen Adams <jalen@jalenkadams.me>
+ *
+ * This file is part of gbt.
+ *
+ * gbt is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * gbt is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gbt.  If not, see <http://www.gnu.org/licenses/>.
+ ***************************************************************************/
+
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
